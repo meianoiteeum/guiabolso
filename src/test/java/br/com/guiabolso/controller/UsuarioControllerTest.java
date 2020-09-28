fd9004df6fe1fa78cc091777dbeed8c744c3e03d
Guilherme Costa <guilherme.costa@itbam.org.br>
2020-09-28 08:50:13 -0400

    feat(test usuario): create class usuario test

---
+++ src/test/java/br/com/guiabolso/controller/UsuarioControllerTest.java
@@ -0,0 +1,33 @@
+package br.com.guiabolso.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsuarioControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private final String url = "usuario";
+
+    void should_ReturnListTransactions_When_GetEndpointListTransactions() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/{url}?count=25&orderASC&page=0&sortProperty=id",url)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[*].id").isNotEmpty());
+    }
+}
