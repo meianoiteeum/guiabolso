b68d67439459e36297c35479f97f1ca2e00ea4cb
guilherme costa <kzaa22@gmail.com>
2020-09-28 20:44:57 -0400

    change README and Swagger

--- src/main/java/br/com/guiabolso/config/SwaggerConfig.java
+++ src/main/java/br/com/guiabolso/config/SwaggerConfig.java
@@ -2,18 +2,10 @@ package br.com.guiabolso.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
 
 @Configuration
 public class SwaggerConfig {
@@ -26,9 +18,4 @@ public class SwaggerConfig {
                 .build();
         return docket;
     }
-
-//    private ApiInfo apiInfo() {
-//        ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service", new Contact("John Doe", "www.example.com", "myeaddress@company.com"), "License of API", "API license URL", Collections.emptyList());
-//        return apiInfo;
-//    }
 }
